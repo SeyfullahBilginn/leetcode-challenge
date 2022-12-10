423799f649f615f6233c82778d104a45f9ecc39a
unknown <seyfullahbilgin06@gmail.com>
2022-12-10 11:12:50 +0300

    add: word-break

---
+++ word-break/solution.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @param {string[]} wordDict
+ * @return {boolean}
+ */
+var wordBreak = function(s, wordDict) {
+  if (!wordDict || wordDict.length == 0) return false
+  var dp = new Array(s.length + 1);
+  dp.fill(false)
+  dp[0] = true
+  
+  for(var i = 1; i <= s.length; i++) {
+      for(var j = 0; j < i; j++) {
+          if(dp[j] && wordDict.indexOf(s.substring(j, i)) >= 0) {
+              console.log(s.substring(j, i));
+              dp[i] = true
+              break;
+          }
+      }
+  }
+  return dp[s.length]
+};
